d9ac39e62357c27f8129e97c540346dd10087e21
humeniucDan <humeniuc.dan.stefan@gmail.com>
2025-03-17 16:00:18 +0200

    added start-up script

---
+++ start-app.ps1
@@ -0,0 +1,13 @@
+# Define the list of directories
+$folders = @("auth", "forum", "user")
+
+# Iterate over each folder
+foreach ($folder in $folders) {
+    if (Test-Path $folder) {
+        Write-Host "Starting docker-compose in folder '$folder' on a separate process..."
+        Start-Process -FilePath "docker-compose" -ArgumentList "up --build" -WorkingDirectory $folder 
+    }
+    else {
+        Write-Host "Folder '$folder' does not exist."
+    }
+}
